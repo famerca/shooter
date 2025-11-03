14d800583a8fb8604002d897de7e54daa9a8b9d0
danyr59 <danyelrange59@gmail.com>
2025-11-03 13:37:44 -0400

    skybox

--- main.cpp
+++ main.cpp
@@ -18,69 +18,14 @@
 #include "Renderer.hpp"
 #include "Input.hpp"
 
-class inputManager: public Input
-{
-    std::shared_ptr<GameObject> object{nullptr};
-    std::shared_ptr<Scene> scene{nullptr};
-    float sensitivity{1.f};
-    float speed{1.f};
-public:
-    inputManager() : Input() {}
-
-    void init(std::shared_ptr<Scene> scene, std::shared_ptr<GameObject> object)
-    {
-        this->object = object;
-        this->scene = scene;
-    }
-
-    void update(const float &dt) noexcept {
-        glm::vec3 pos = object->getTransform()->getPosition();
-
-        if(is_key_pressed(GLFW_KEY_W))
-        {
-            object->getTransform()->translate(pos + glm::vec3(0.f, 0.f, 1.f) * speed * dt);
-        }
-
-        if(is_key_pressed(GLFW_KEY_S))
-        {
-            object->getTransform()->translate(pos + glm::vec3(0.f, 0.f, -1.f) * speed * dt);
-        }  
-
-        if(is_key_pressed(GLFW_KEY_A))
-        {
-            object->getTransform()->translate(pos + glm::vec3(1.f, 0.f, 0.f) * speed * dt);
-        }
-
-        if(is_key_pressed(GLFW_KEY_D))
-        {
-            object->getTransform()->translate(pos + glm::vec3(-1.f, 0.f, 0.f) * speed * dt);
-        }
-
-        std::cout << "delta: " << dt <<  ", FPS: " << 1.f / dt << std::endl;
-
-        MousePair delta = this->get_mouse_delta();
-        if(delta.x != 0.f || delta.y != 0.f)
-        {
-           // std::cout << "x: " << delta.x << ", y: " << delta.y << std::endl;
-            auto camera = scene->getCamera();
-            if(camera != nullptr)
-            {
-                camera->setRotation(-delta.x * sensitivity, -delta.y * sensitivity);
-            }
-        }
-
-    }
-
-    ~inputManager() {}
-};
 
 int main()
 {
     try
     {
         // Window dimensions
-        constexpr GLint WIDTH = 1200;
-        constexpr GLint HEIGHT = 720;
+        constexpr GLint WIDTH = 800;
+        constexpr GLint HEIGHT = 600;
 
         auto main_window = Window::create(WIDTH, HEIGHT, "Cubo y piramide");
 
@@ -88,7 +33,7 @@ int main()
         {
             return EXIT_FAILURE;
         }
-        auto input = std::make_shared<inputManager>();
+        auto input = Input::create();
         main_window->setInput(input);
 
         auto light = std::make_shared<DirectionalLight>(1.f, 1.f, 1.f, 1.f, 0.f, 1.f, 0.5f);
@@ -97,14 +42,7 @@ int main()
         auto cameraObject = scene->createGameObject();
         auto camera = scene->createCamera(cameraObject);
 
-        scene->at(cameraObject)->getTransform()->translate(glm::vec3(0.f, 0.f, -5.f));
-        
-        camera->setFront(0.f, 90.f);
-        camera->setUp(glm::vec3(0.f, 1.f, 0.f));
-
-        input->init(scene, scene->at(cameraObject));
-
-        camera->init(glm::vec3(0.f, 0.f, 0.f), main_window->get_aspect_ratio(), 45.f, 0.1f, 100.f, true);
+        camera->init(glm::vec3(0.f, 0.f, 0.f), main_window->get_aspect_ratio(), 45.f, 0.1f, 100.f, false);
         camera->activate();
 
         auto suzanne = scene->createGameObject();
@@ -117,6 +55,9 @@ int main()
         scene->at(sphere)->getTransform()->scale(0.4f, 0.4f, 0.4f);
         scene->at(suzanne)->getTransform()->translate(-1.f, 0.f, -2.5f);
         scene->at(suzanne)->getTransform()->scale(0.4f, 0.4f, 0.4f);
+
+        auto skybox = Skybox::create();
+        scene->setSkybox(skybox);
 
         auto renderer = std::make_shared<Renderer>();
         renderer->init();
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -123,3 +123,45 @@ void Renderer::calcDeltaTime()
     delta_time = static_cast<GLfloat>(current_time - last_frame_time);
     last_frame_time = current_time;
 }
+
+void Renderer::render_pass(const glm::mat4& projection, const glm::mat4& view) noexcept
+{
+    glViewport(0, 0, Data::WIDTH, Data::HEIGHT);
+
+    // Clear the window
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    sky_box->render(view, projection);
+
+    
+/*
+    Data::shader_list[0]->use();
+    Data::uniform_model_id = Data::shader_list[0]->get_uniform_model_id();
+    Data::uniform_projection_id = Data::shader_list[0]->get_uniform_projection_id();
+    Data::uniform_view_id = Data::shader_list[0]->get_uniform_view_id();
+    Data::uniform_eye_position_id = Data::shader_list[0]->get_uniform_eye_position_id();
+    Data::uniform_specular_intensity_id = Data::shader_list[0]->get_uniform_specular_intensity_id();
+    Data::uniform_shininess_id = Data::shader_list[0]->get_uniform_specular_shininess_id();
+
+    glUniformMatrix4fv(Data::shader_list[0]->get_uniform_projection_id(), 1, GL_FALSE, glm::value_ptr(projection));
+    glUniformMatrix4fv(Data::shader_list[0]->get_uniform_view_id(), 1, GL_FALSE, glm::value_ptr(view));
+    glUniform3f(Data::shader_list[0]->get_uniform_eye_position_id(), Data::camera->get_position().x, Data::camera->get_position().y, Data::camera->get_position().z);
+
+    Data::shader_list[0]->set_directional_light(Data::main_light);
+    Data::shader_list[0]->set_point_lights(Data::point_lights, 3, 0);
+    Data::shader_list[0]->set_spot_lights(Data::spot_lights, 3 + Data::point_lights.size(), Data::point_lights.size());
+    Data::shader_list[0]->set_directional_light_space_transform(Data::main_light->get_light_transform());
+
+    Data::main_light->get_shadow_map()->read(GL_TEXTURE2);
+    Data::shader_list[0]->set_texture(1);
+    Data::shader_list[0]->set_directional_shadow_map(2);
+
+    auto lower_light = Data::camera->get_position();
+    lower_light.y -= 0.3f;
+    //Data::spot_lights[0]->set(lower_light, Data::camera->get_direction());
+
+    render_scene();
+
+    */
+}
